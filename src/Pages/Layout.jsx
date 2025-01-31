2f7827da5de433185d699ff9950391110a031047
rakib1248 <admin@rakib.com>
2025-01-31 22:11:01 +0300

    first commit

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,30 +1,18 @@
-import { useEffect, useRef } from "react";
 import Chatmassege from "./chat/Chat";
 import ChatInput from "./chat/ChatInput";
-
 import Header from "./chat/header";
 
 const Layout = () => {
-    const messagesEndRef = useRef(null);
-   useEffect     (() => {
-     messagesEndRef.current?.scrollIntoView({
-       behavior: "smooth",
-       block: "end",
-     });
-   }, []);
- 
   return (
     <div className="w-full">
-      <div className="fixed h-screen flex  flex-col justify-between container ">
-        <div className="bg-amber-200 h-[75px]">
+      <div className="fixed h-screen flex flex-col justify-between">
+        <div className="bg-amber-200 h-[75px] flex-shrink-0">
           <Header />
         </div>
-        <div
-          ref={messagesEndRef}
-          className="bg-[url('../../Screenshot_1.jpg')] bg-cover bg-center h-[95%] overflow-auto px-3 py-2">
+        <div className="bg-[url('../../Screenshot_1.jpg')] bg-cover bg-center flex-grow overflow-y-auto px-3 py-2">
           <Chatmassege />
         </div>
-        <div className=" h-[70px]">
+        <div className="h-[70px] flex-shrink-0">
           <ChatInput />
         </div>
       </div>
