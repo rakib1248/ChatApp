60358f80edf7a9ce16be02bc2a8aba3b3b1c523d
rakib1248 <admin@rakib.com>
2025-02-01 13:19:02 +0300

    update

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,44 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import LogineRagister from "../Pages/LogineRagister";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "./userContest";
 import Layout from "../Pages/Layout";
 
 const AppRouter = () => {
-  const { user } = useContext(UserContext);
+  const { user, setSingleUser, setUser } = useContext(UserContext);
+  const userLocal = () => {
+    try {
+      const userData = JSON.parse(localStorage.getItem("user"));
 
+      if (userData) {
+        setSingleUser(userData);
+        setUser(true);
+      } else {
+        setUser(false);
+        setSingleUser({});
+      }
+    } catch (error) {
+      console.error("Error parsing user data:", error);
+      setUser(false);
+      setSingleUser({});
+    }
+  };
+
+useEffect(() => {
+  userLocal();
+  const handleStorageChange = () => {
+    userLocal();
+  };
+  window.addEventListener("storage", handleStorageChange);
+
+  return () => {
+    window.removeEventListener("storage", handleStorageChange);
+  };
+}, []);
+
+
+  // const [islogine, setIslogine] = useState(false);
 
   return (
     <>
